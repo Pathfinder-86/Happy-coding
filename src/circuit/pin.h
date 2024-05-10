b3612a4565060a26e16c2fa72a04fff32fd1d831
James Hsu <james891115@gmail.com>
2024-05-10 22:41:31 +0800

    2024/5/10 Add basic circuit

---
+++ src/circuit/pin.h
@@ -0,0 +1,20 @@
+#ifndef CIRCUIT_PIN_H
+#define CIRCUIT_PIN_H
+namespace circuit {
+class Pin {
+public:
+    Pin(){};
+    Pin(int cell_id, int net_id) : cell_id(cell_id), net_id(net_id) {}
+    int get_id() const { return id; }
+    int get_cell_id() const { return cell_id; }
+    int get_net_id() const { return net_id; }
+    void set_id(int id) { this->id = id; }
+    void set_cell_id(int cell_id) { this->cell_id = cell_id; }
+    void set_net_id(int net_id) { this->net_id = net_id; }
+private:
+    int id;
+    int cell_id;
+    int net_id;
+};
+}
+#endif // PIN_H
