4736041df1ae23fa37448493d2a12832b0709314
James Hsu <james891115@gmail.com>
2024-08-12 14:57:35 +0800

    unfinish legalization

--- src/legalizer/legalizer.cpp
+++ src/legalizer/legalizer.cpp
@@ -1,15 +1,20 @@
 #include "legalizer.h"
 #include "../circuit/netlist.h"
 #include "../circuit/cell.h"
+#include <unordered_set>
+#include <algorithm>
+
 namespace legalizer{
 
 bool Legalizer::check_on_site(){
     const circuit::Netlist &netlist = circuit::Netlist::get_instance();
     const std::vector<circuit::Cell> &cells = netlist.get_cells();
-    for(const circuit::Cell &cell : cells){
-        double x = cell.get_x();
-        double y = cell.get_y();                
-        if(sites_set.find(std::make_pair(x,y)) == sites_set.end()){
+    const std::unordered_set<int> &sequential_cells_id = netlist.get_sequential_cells_id();
+    for(int cid : sequential_cells_id){
+        const circuit::Cell &cell = cells.at(cid);
+        int x = cell.get_x();
+        int y = cell.get_y();
+        if(sites_xy_to_id_map.find(std::make_pair(x,y)) == on_sites_set.end()){
             int cid = cell.get_id();
             const std::string &cell_name = netlist.get_cell_name(cid);
             std::cerr << "Cell " << cell_name << "(x=" << x << ",y=" << y << ") is not on site." << std::endl;
@@ -19,6 +24,135 @@ bool Legalizer::check_on_site(){
     return true;
 }
 
+void Legalizer::init_blockage(){
+    const circuit::Netlist &netlist = circuit::Netlist::get_instance();
+    const std::vector<circuit::Cell> &cells = netlist.get_cells();
+    for(const circuit::Cell &cell : cells){
+        if(cell.is_sequential()){
+            continue;
+        }
+        int x = cell.get_x();
+        int y = cell.get_y();
+        int rx = cell.get_rx();
+        int ry = cell.get_ry();
+        // remove overlapping sites
+        for(auto it = sites.begin(); it != sites.end();){
+            int sx = it->get_x();
+            int sy = it->get_y();
+            int srx = it->get_rx();
+            int sry = it->get_ry();
+            // if not overlap
+            if(rx <= sx || ry <= sy || x >= srx || y >= sry){
+                it++;
+                continue;
+            }else{
+                it = sites.erase(it);
+                //std::cout<<"Blockage at site ("<<sx<<","<<sy<<")"<<std::endl;
+            }                        
+        }
+        
+    }
+    for(int site_id = 0; site_id < sites.size(); site_id++){        
+        empty_sites_id.insert(site_id);
+        sites_id_to_xy_map[site_id] = std::make_pair(sites.at(site_id).get_x(),sites.at(site_id).get_y());
+        sites_xy_to_id_map[std::make_pair(sites.at(site_id).get_x(),sites.at(site_id).get_y())] = site_id; 
+    }
+}
+
+void Legalizer::find_unplaced_cells_and_empty_sites(){
+    const circuit::Netlist &netlist = circuit::Netlist::get_instance();
+    const std::vector<circuit::Cell> &cells = netlist.get_cells();
+    const std::unordered_set<int> &sequential_cells_id = netlist.get_sequential_cells_id();
+    not_on_site_cells_id = sequential_cells_id;
+    for(int cid : sequential_cells_id){
+        const circuit::Cell &cell = cells.at(cid);
+        int x = cell.get_x();
+        int y = cell.get_y();
+        int rx = cell.get_rx();
+        int ry = cell.get_ry();
+        // mark overlapping sites empty to false
+
+        for(auto it = empty_sites_id.begin() ; it != empty_sites_id.end();){
+            int site_id = *it;
+            const Site &site = sites.at(site_id);
+            int sx =  site.get_x();
+            int sy =  site.get_y();
+            int srx = site.get_rx();
+            int sry = site.get_ry();
+            // if not overlap
+            if(rx <= sx || ry <= sy || x >= srx || y >= sry){
+                it++;                
+                continue;
+            }else{
+                it = empty_sites_id.erase(it);
+                site_id_to_cell_id_map[site_id] = cid;
+                cell_id_to_site_id_map[cid] = site_id;
+                not_on_site_cells_id.erase(cid);
+
+                //const std::string &cell_name = netlist.get_cell_name(cid);
+                //std::cout << "Cell " << cell_name << " at site (" << sx << "," << sy << ")" << std::endl;
+            }                 
+        }
+    }
+}
+
+void Legalizer::print_empty_sites() const{
+    for(int site_id : empty_sites_id){
+        const Site &site = sites.at(site_id);
+        std::cout << "Empty site (" << site.get_x() << "," << site.get_y() << ")" << std::endl;
+    }
+}
+
+
+bool Legalizer::legalize(){
+    if(!available){
+        return false;
+    }
+
+    const circuit::Netlist &netlist = circuit::Netlist::get_instance();
+    const std::vector<circuit::Cell> &cells = netlist.get_cells();
+    for(int cid : not_on_site_cells_id){
+        const circuit::Cell &cell = cells.at(cid);
+        int x = cell.get_x();
+        int y = cell.get_y();
+        int rx = cell.get_rx();
+        int ry = cell.get_ry();
+        // find the nearest empty site
+        std::vector<std::pair<int,double>> cell_sites_distances;
+        for(int site_id : empty_sites_id){
+            const Site &site = sites.at(site_id);
+            int sx = site.get_x();
+            int sy = site.get_y();
+            int srx = site.get_rx();
+            int sry = site.get_ry();
+            double dist = abs(x - sx) + abs(y - sy);
+            cell_sites_distances.push_back(std::make_pair(site_id,dist));
+        }
+
+        std::sort(cell_sites_distances.begin(),cell_sites_distances.end(),[](const std::pair<int,double> &a, const std::pair<int,double> &b){
+            return a.second < b.second;
+        });
+        
+        if(cell_sites_distances.empty()){
+            std::cerr << "No available site for cell " << netlist.get_cell_name(cid) << std::endl;
+            return false;
+        }
+        // cells may larger than sites
+        // need mutiple sites to place a cell
+        // try to place the cell until success or return false
+        for(const auto &it : cell_sites_distances){
+            int site_id = it.first;
+            const Site &site = sites.at(site_id);
+            int sx = site.get_x();
+            int sy = site.get_y();
+            int srx = site.get_rx();
+            int sry = site.get_ry();
+            if(rx )
+        }
+
+        
+    }
+}
 
 
 
--- src/legalizer/legalizer.h
+++ src/legalizer/legalizer.h
@@ -3,9 +3,12 @@
 #include <set>
 #include <vector>
 #include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
 namespace legalizer {
 
-struct Row {
+/*struct Row {
     Row(int x, int y, int width, int height): x(x), y(y), width(width), height(height){}
     int x,y;
     int width,height;
@@ -21,56 +24,86 @@ struct Row {
     int get_ry(){
         return y + height;
     }
-};
+};*/
 
 struct Site {
     Site(int x, int y, int width, int height): x(x), y(y), width(width), height(height){}
     int x,y;
     int width,height;
-    int get_x(){
+    int get_x() const{
         return x;
     }
-    int get_y(){
+    int get_y() const{
         return y;
     }
-    int get_rx(){
+    int get_rx() const{
         return x + width;
     }
-    int get_ry(){
+    int get_ry() const{
         return y + height;
     }
 };
 
 class Legalizer{
     public:
-        Legalizer(){}
+        Legalizer(): site_height(-1),site_width(-1),available(true){}
         static Legalizer& get_instance() {
             static Legalizer instance;        
             return instance;
         }
         void add_row(int x, int y, int site_width, int site_height, int site_num){
-            rows.push_back(Row{x,y,site_width*site_num,site_height});
+            //rows.push_back(Row{x,y,site_width*site_num,site_height});
             for(int i = 0; i < site_num; i++){
                 sites.push_back(Site(x + i*site_width, y, site_width, site_height));
                 //std::cout << "Site " << x + i*site_width << "," << y << std::endl;
-                sites_set.insert(std::make_pair(x + i*site_width, y));
+                on_sites_set.insert(std::make_pair(x + i*site_width, y));
+                site_id_map[std::make_pair(x + i*site_width, y)] = sites.size()-1;
+            }
+
+            if(site_height == -1){
+                site_height = site_height;
+                site_width = site_width;
+            }else{
+                if(site_height != site_height || site_width != site_width){
+                    std::cerr << "Site height and width should be the same for all rows" << std::endl;
+                    available = false;
+                }
             }
         }
         void init(){
-            init_rows = rows;
+            init_blockage();
             init_sites = sites;
+            find_unplaced_cells_and_empty_sites();
+            //init_rows = rows;
+            print_empty_sites();
         }
         void reset(){
-            rows = init_rows;
+            //rows = init_rows;
             sites = init_sites;
         }
         bool check_on_site();
+        bool legalize();
+        void init_blockage();
+        void find_unplaced_cells_and_empty_sites();
+        void print_empty_sites() const;
     private:   
-        std::vector<Row> rows;
-        std::vector<Site> sites;
-        std::vector<Row> init_rows;
+        //std::vector<Row> rows;
+        //std::vector<Row> init_rows;
+        
         std::vector<Site> init_sites;
-        std::set<std::pair<int,int>> sites_set;
+        // quick site access, check
+        std::vector<Site> sites;   
+        std::unordered_map<int,std::pair<int,int>> sites_id_to_xy_map;
+        std::map<std::pair<int,int>,int> sites_xy_to_id_map;
+        std::unordered_set<int> empty_sites_id;
+
+        // site cell relation
+        std::unordered_set<int> not_on_site_cells_id;
+        std::unordered_map<int,int> site_id_to_cell_id_map;
+        std::unordered_map<int,int> cell_id_to_site_id_map;
+
+        int site_width, site_height;
+        bool available;
 };
 
 }
